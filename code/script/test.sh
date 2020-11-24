ca3011cab40b193970a34475dac1af3b1f56e9ce
shiroha <ymd45921@163.com>
2020-11-24 16:30:55 +0800

    改进了测试脚本和样例

--- code/script/test.sh
+++ code/script/test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# TODO: Need to repair
-set -e
 
 tests=$(dirname $(readlink -f $0))/../test
 build=$(dirname $(readlink -f $0))/../build
@@ -10,21 +8,19 @@ if !(test -f $build/toy_compiler); then
     echo "Binary is not found, please build first."
     exit 1
 fi  
-if !(test -d $tests/out); then
-    mkdir $tests/out
-fi
-if !(test -d $tests/err); then
-    mkdir $tests/err
-fi
 if !(test -d $out); then
     mkdir $out
 fi
-for ii in $(seq 1 20)
-do 
-    if test -f `$tests/test$ii.txt`; then
-        (cd $out && $build/toy_compiler `$tests/test$ii.txt` > `$tests/out/test$ii.out` 2> `$tests/err/test$ii.err`)
-    else 
-        echo "Test completed"    
-        break
-    fi
-done            
+for ii in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+do
+    echo "Info: Test $ii start:"
+    echo ""
+    (cd $out && $build/toy_compiler $tests/test$ii.txt) 
+    if $? != "0"; then
+        echo "Warn: Return value is not 0."
+    fi    
+    echo ""
+    echo "Info: Test $ii end."
+    echo "" 
+done  
+echo "Info: AST, symbol table and LLVM IR code is in '$out'"      
