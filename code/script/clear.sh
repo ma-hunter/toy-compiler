e4fe1c6166efd1bd2b6dbec47647aa9805c6fb47
shiroha <ymd45921@163.com>
2020-11-24 17:09:04 +0800

    增加了 y.output

--- code/script/clear.sh
+++ code/script/clear.sh
@@ -2,7 +2,7 @@
 set -e
 
 me=$(dirname $(readlink -f $0))
-rm -f $me/../src/lex.yy.cc $me/../include/parser.tab.h $me/../src/parser.tab.cc
+rm -f $me/../src/lex.yy.cc $me/../include/parser.tab.h $me/../src/parser.tab.cc $me/../config.y.output
 rm -rf $me/../build/ $me/../cmake
 rm -rf $me/../test/log/ $me/../test/err/ $me/../test/out/ $me/../out/
 
--- code/script/generate.sh
+++ code/script/generate.sh
@@ -5,5 +5,7 @@ me=$(dirname $(readlink -f $0))
 
 flex -o $me/../src/lex.yy.cc $me/../config/lex.l
 bison --defines=$me/../include/parser.tab.h --output=$me/../src/parser.tab.cc $me/../config/parser.y
+(cd $me/../config && bison -y -v parser.y > out 2> err)
+(cd $me/../config && rm -f y.tab.c out err)
 
 echo "Syntax analyser and parser generated successfully."
